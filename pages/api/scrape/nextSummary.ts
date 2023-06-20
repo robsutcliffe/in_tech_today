a6f622d0f9fc5aa2bc0f64cae58293cf861ec081
robsutcliffe <rob@firefields.com>
2023-06-20 08:58:01 +0100

    reduce maximum character count

--- pages/api/scrape/nextSummary.ts
+++ pages/api/scrape/nextSummary.ts
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       const { html } = await getBlogPost(post.href);
       const nextPost = { ...post, summary: [], tags: [] };
 
-      if (isMostlyEnglish(html) && html.length < 35000) {
+      if (isMostlyEnglish(html) && html.length < 20000) {
         const fullResponse = await summarise(html);
         const parts = fullResponse.content.split("#");
         nextPost.summary = parts[0]
