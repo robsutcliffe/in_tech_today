aee519d54ec9342dbbb94edde2ec26722b21f683
robsutcliffe <rob@firefields.com>
2023-08-23 12:07:45 +0200

    add aws amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import "@styles/globals.css";
 
 import { Amplify } from "aws-amplify";
 import awsExports from "../src/aws-exports";
-Amplify.configure({ ...awsExports });
+Amplify.configure({ ...awsExports, ssr: true });
 
 const MyApp: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   return (
